e1dda9f181b01380cc2a9386a377ddf4234f10f6
RuiOkazaki <okazaki200226@gmail.com>
2022-11-25 00:16:42 +0900

    feat: userのtokenとuuidを格納するclassを作成

---
+++ lib/models/user/user_model.g.dart
@@ -0,0 +1,17 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_User _$$_UserFromJson(Map<String, dynamic> json) => _$_User(
+      uuid: json['uuid'] as String?,
+      token: json['token'] as String?,
+    );
+
+Map<String, dynamic> _$$_UserToJson(_$_User instance) => <String, dynamic>{
+      'uuid': instance.uuid,
+      'token': instance.token,
+    };
