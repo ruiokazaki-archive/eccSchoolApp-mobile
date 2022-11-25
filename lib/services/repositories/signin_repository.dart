39927f70f1965a7427ea14f6fc8122d89df4cd6d
RuiOkazaki <okazaki200226@gmail.com>
2022-11-25 15:13:22 +0900

    feat: authに関するrepositoryを作成

--- lib/services/repositories/signin_repository.dart
+++
@@ -1,8 +0,0 @@
-import 'package:ecc_school_app_mobile/services/networking/api_client.dart';
-
-class SinginRepository {
-  final _api = ApiClient();
-  dynamic signin() async {
-    return _api.signin();
-  }
-}
