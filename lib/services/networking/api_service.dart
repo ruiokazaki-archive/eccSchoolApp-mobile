830f2d89a68756ba31911ec0d06b2a6051555b27
RuiOkazaki <okazaki200226@gmail.com>
2022-12-05 18:58:56 +0900

    feat(#20): api serviceにattendanceを実装した

--- lib/services/networking/api_service.dart
+++ lib/services/networking/api_service.dart
@@ -1,4 +1,5 @@
 import 'package:ecc_school_app_mobile/helpers/typedefs.dart';
+import 'package:ecc_school_app_mobile/models/attendance/attendance_model.dart';
 import 'package:ecc_school_app_mobile/models/user/user_auth_model.dart';
 import 'package:ecc_school_app_mobile/services/networking/api_endpoint.dart';
 import 'package:ecc_school_app_mobile/services/networking/dio_service.dart';
@@ -14,5 +15,20 @@ class ApiService {
     );
     return userAuth;
   }
+
+  Future<List<Attendance>> attendance(UserAuth userAuth) async {
+    if (userAuth.isNull) {
+      throw Exception('User is not signed in');
+    }
+
+    final attendances = await _dioService.getCollection<Attendance>(
+      endpoint: ApiEndpoint.user(
+        UserEndpoint.ATTENDANCE,
+        uuid: userAuth.uuid!,
+      ),
+      token: userAuth.token!,
+      converter: (responseBody) => Attendance.fromJson(responseBody),
+    );
+    return attendances;
   }
 }
