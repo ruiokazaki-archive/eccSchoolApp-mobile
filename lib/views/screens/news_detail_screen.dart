5f23af25683a1c25a41c35df80f6fdb7b4075bc0
RuiOkazaki <okazaki200226@gmail.com>
2023-01-03 21:37:54 +0900

    feat(#38): お知らせ詳細画面を作成した

--- lib/views/screens/news_detail_screen.dart
+++ lib/views/screens/news_detail_screen.dart
@@ -1,5 +1,7 @@
+import 'package:ecc_school_app_mobile/helpers/utils/open_url.dart';
 import 'package:ecc_school_app_mobile/providers/news_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_html/flutter_html.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -10,14 +12,34 @@ class NewsDetailScreen extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final newsDetail = ref.watch(newsDetailNotifierProvider);
-    ref.read(newsDetailNotifierProvider.notifier).getNewsDetail(newsId);
+
+    useEffect(() {
+      ref.read(newsDetailNotifierProvider.notifier).getNewsDetail(newsId);
+      return null;
+    }, [newsId]);
 
     return Scaffold(
       appBar: AppBar(title: Text(newsDetail[0].title)),
       body: SingleChildScrollView(
         child: Html(
-            data:
-                "<h2>${newsDetail[0].title}</h2><p>${newsDetail[0].date}</p>${newsDetail[0].body}<br />${newsDetail[0].attachment?.asMap().entries.map((entry) => '<a src=${entry.value}>添付ファイル${entry.key + 1}</a>').join()}"),
+          onLinkTap: (url, _, __, ___) {
+            openUrl(url);
+          },
+          tagsList: Html.tags..addAll(["attachment"]),
+          customRender: {
+            "attachment": (RenderContext context, Widget child) {
+              return ElevatedButton(
+                onPressed: () {
+                  debugPrint(context.tree.element!.attributes["href"]!);
+                  openUrl(context.tree.element!.attributes["href"]!);
+                },
+                child: Text(context.tree.element!.innerHtml),
+              );
+            },
+          },
+          data:
+              "<h2>${newsDetail[0].title}</h2><p>${newsDetail[0].date}</p>${newsDetail[0].body}<br />${newsDetail[0].attachment?.asMap().entries.map((entry) => '<attachment href="${entry.value}">添付ファイル${entry.key + 1}</attachment>').join() ?? ""}",
+        ),
       ),
     );
   }
