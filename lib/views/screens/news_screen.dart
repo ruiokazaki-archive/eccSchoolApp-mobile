8151b7aea102741e26356167126729b0f95e2a5a
RuiOkazaki <okazaki200226@gmail.com>
2023-01-03 17:55:20 +0900

    feat(#38): お知らせ一覧画面から詳細ページへ遷移できるよう

--- lib/views/screens/news_screen.dart
+++ lib/views/screens/news_screen.dart
@@ -2,6 +2,7 @@ import 'package:ecc_school_app_mobile/helpers/utils/news_colors_generator.dart';
 import 'package:ecc_school_app_mobile/providers/news_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:ecc_school_app_mobile/routes/routes.dart' as routes;
 
 class NewsScreen extends HookConsumerWidget {
   const NewsScreen({Key? key}) : super(key: key);
@@ -15,45 +16,52 @@ class NewsScreen extends HookConsumerWidget {
       body: ListView.separated(
         itemCount: news.length,
         itemBuilder: (context, index) {
-          return Container(
-            padding: const EdgeInsets.all(8),
-            child: Row(
-              children: [
-                Container(
-                  padding: const EdgeInsets.only(right: 12.0),
-                  child: CircleAvatar(
-                    backgroundColor:
-                        NewsColorsGenerator.getColor(news[index].tag),
-                    radius: 16.0,
+          return ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              foregroundColor: Colors.black87,
+              backgroundColor: Colors.white,
+            ),
+            onPressed: () => routes.NewsDetailRoute(news[index].id).go(context),
+            child: Container(
+              padding: const EdgeInsets.all(8),
+              child: Row(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.only(right: 12.0),
+                    child: CircleAvatar(
+                      backgroundColor:
+                          NewsColorsGenerator.getColor(news[index].tag),
+                      radius: 16.0,
+                    ),
                   ),
-                ),
-                Flexible(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        children: [
-                          Text(
-                            news[index].tag,
-                            style: const TextStyle(fontSize: 12),
-                          ),
-                          const Spacer(),
-                          Text(
-                            news[index].date,
-                            style: const TextStyle(fontSize: 10),
-                          ),
-                        ],
-                      ),
-                      const SizedBox(height: 8),
-                      Text(
-                        news[index].title,
-                        maxLines: 1,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                    ],
+                  Flexible(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            Text(
+                              news[index].tag,
+                              style: const TextStyle(fontSize: 12),
+                            ),
+                            const Spacer(),
+                            Text(
+                              news[index].date,
+                              style: const TextStyle(fontSize: 10),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 8),
+                        Text(
+                          news[index].title,
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           );
         },
