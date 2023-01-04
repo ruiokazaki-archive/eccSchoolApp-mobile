a924c72c7f5ed718b807e32038bb7e84d323667c
RuiOkazaki <okazaki200226@gmail.com>
2023-01-04 16:03:50 +0900

    feat(#42): calendar state のデータ構造を変更した

--- lib/providers/calendar_provider.dart
+++ lib/providers/calendar_provider.dart
@@ -1,14 +1,17 @@
-import 'package:ecc_school_app_mobile/models/calendar/calendar_model.dart';
 import 'package:ecc_school_app_mobile/providers/auth_provider.dart';
 import 'package:ecc_school_app_mobile/services/repositories/calendar_repository.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:intl/intl.dart';
 
 final calendarNotifierProvider =
-    StateNotifierProvider<CalendarNotifier, List<CalendarDay>>(
+    StateNotifierProvider<CalendarNotifier, dynamic>(
         (ref) => CalendarNotifier(ref));
 
-class CalendarNotifier extends StateNotifier<List<CalendarDay>> {
-  CalendarNotifier(this.ref) : super([]);
+class CalendarNotifier extends StateNotifier<dynamic> {
+  CalendarNotifier(this.ref) : super({}) {
+    DateTime now = DateTime.now();
+    getCalendar(DateFormat('yyyy').format(now), DateFormat('MM').format(now));
+  }
 
   final calendarRepository = CalendarRepository();
   final Ref ref;
@@ -21,9 +24,10 @@ class CalendarNotifier extends StateNotifier<List<CalendarDay>> {
 
     if (userAuth == null) return;
 
+    if (state["$year$month"] != null) return;
+
     final calendar =
         await calendarRepository.getCalendar(userAuth, year, month);
-
-    state = calendar;
+    state = {...state, "$year$month": calendar};
   }
 }
