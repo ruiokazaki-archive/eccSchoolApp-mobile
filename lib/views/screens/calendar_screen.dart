10bc261aa202c8b9a194a1da73149868706b8024
RuiOkazaki <okazaki200226@gmail.com>
2023-01-05 18:30:35 +0900

    feat(#42): 翌月，前月に遷移したタイミングでデータをfetchするようにした

--- lib/views/screens/calendar_screen.dart
+++ lib/views/screens/calendar_screen.dart
@@ -4,6 +4,7 @@ import 'package:ecc_school_app_mobile/providers/calendar_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:intl/intl.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 Widget _buildEventsMarker(DateTime date, List events) {
@@ -80,6 +81,10 @@ class CalendarScreen extends HookConsumerWidget {
               }
             },
             onPageChanged: (focusedDay) {
+              ref.read(calendarNotifierProvider.notifier).getCalendar(
+                    DateFormat('yyyy').format(focusedDay),
+                    DateFormat('MM').format(focusedDay),
+                  );
               focusedDayState.value = focusedDay;
             },
             calendarBuilders: CalendarBuilders(
