6f3932c08d3194bdc8fe8396a34f2fc51bf71d59
RuiOkazaki <okazaki200226@gmail.com>
2023-01-26 02:38:32 +0900

    feat: #53 UIを作成した

--- lib/views/screens/sign_in_screen.dart
+++ lib/views/screens/sign_in_screen.dart
@@ -1,8 +1,13 @@
+import 'package:ecc_school_app_mobile/helpers/utils/open_url.dart';
 import 'package:ecc_school_app_mobile/providers/auth_provider.dart';
+import 'package:ecc_school_app_mobile/views/widgets/sign_in/wave_clipper1.dart';
+import 'package:ecc_school_app_mobile/views/widgets/sign_in/wave_clipper2.dart';
+import 'package:ecc_school_app_mobile/views/widgets/sign_in/wave_clipper3.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:ecc_school_app_mobile/routes/routes.dart' as routes;
 
 class SignInScreen extends HookConsumerWidget {
   const SignInScreen({super.key});
@@ -44,139 +49,216 @@ class SignInScreen extends HookConsumerWidget {
       }
     }
 
-    Container _buildLoginForm() {
-      return Container(
-        padding: const EdgeInsets.all(20.0),
-        child: Stack(
-          children: <Widget>[
-            ClipPath(
-              clipper: RoundedDiagonalPathClipper(),
-              child: Container(
-                height: 400,
-                padding: const EdgeInsets.all(10.0),
-                decoration: const BoxDecoration(
-                  borderRadius: BorderRadius.all(Radius.circular(40.0)),
-                  color: Colors.white,
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    const SizedBox(
-                      height: 90.0,
-                    ),
-                    Container(
-                        padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                        child: TextField(
-                          style: const TextStyle(color: Colors.blue),
-                          decoration: InputDecoration(
-                              hintText: "Email address",
-                              hintStyle: TextStyle(color: Colors.blue.shade200),
-                              border: InputBorder.none,
-                              icon: const Icon(
-                                Icons.email,
-                                color: Colors.blue,
-                              )),
-                        )),
-                    Container(
-                      padding: const EdgeInsets.only(
-                          left: 20.0, right: 20.0, bottom: 10.0),
-                      child: Divider(
-                        color: Colors.blue.shade400,
-                      ),
-                    ),
-                    Container(
-                        padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                        child: TextField(
-                          style: const TextStyle(color: Colors.blue),
-                          decoration: InputDecoration(
-                              hintText: "Password",
-                              hintStyle: TextStyle(color: Colors.blue.shade200),
-                              border: InputBorder.none,
-                              icon: const Icon(
-                                Icons.lock,
-                                color: Colors.blue,
-                              )),
-                        )),
-                    Container(
-                      padding: const EdgeInsets.only(
-                          left: 20.0, right: 20.0, bottom: 10.0),
-                      child: Divider(
-                        color: Colors.blue.shade400,
-                      ),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: <Widget>[
-                        Container(
-                            padding: const EdgeInsets.only(right: 20.0),
-                            child: const Text(
-                              "Forgot Password",
-                              style: TextStyle(color: Colors.black45),
-                            ))
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(0),
+        child: AppBar(
+          elevation: 0.0,
+        ),
+      ),
+      body: Column(
+        children: [
+          Stack(
+            children: <Widget>[
+              ClipPath(
+                clipper: WaveClipper2(),
+                child: Container(
+                  width: double.infinity,
+                  height: 300,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        Theme.of(context).primaryColor.withOpacity(0.1),
+                        Theme.of(context).primaryColor.withOpacity(0.2),
                       ],
                     ),
-                    const SizedBox(
-                      height: 10.0,
-                    ),
-                  ],
+                  ),
                 ),
               ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                CircleAvatar(
-                  radius: 40.0,
-                  backgroundColor: Colors.blue.shade600,
-                  child: const Icon(Icons.person),
+              ClipPath(
+                clipper: WaveClipper3(),
+                child: Container(
+                  width: double.infinity,
+                  height: 300,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        Theme.of(context).primaryColor.withOpacity(0.2),
+                        Theme.of(context).primaryColor.withOpacity(0.4),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              ClipPath(
+                clipper: WaveClipper1(),
+                child: Container(
+                  width: double.infinity,
+                  height: 300,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        Theme.of(context).primaryColor,
+                        Theme.of(context).primaryColor,
+                      ],
+                    ),
+                  ),
+                  child: Column(
+                    children: const <Widget>[
+                      SizedBox(
+                        height: 40,
+                      ),
+                      Icon(
+                        Icons.logo_dev,
+                        color: Colors.white,
+                        size: 60,
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        "app name?",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w700,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          Form(
+            key: formKey,
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 32),
+                  child: Material(
+                    elevation: 2.0,
+                    borderRadius: const BorderRadius.all(Radius.circular(30)),
+                    child: TextFormField(
+                      controller: userIdController,
+                      validator: (value) =>
+                          formValidator(value, 'Student number'),
+                      keyboardType: TextInputType.number,
+                      inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                      onFieldSubmitted: (_) => onSubmitHandler(
+                        () => const routes.HomeRoute().go(context),
+                      ),
+                      decoration: InputDecoration(
+                        labelText: 'Student number',
+                        hintText: 'Your student number',
+                        prefixIcon: Material(
+                          elevation: 0,
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(30)),
+                          child: Icon(
+                            Icons.person,
+                            color: Theme.of(context).primaryColor,
+                          ),
+                        ),
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 32),
+                  child: Material(
+                    elevation: 2.0,
+                    borderRadius: const BorderRadius.all(Radius.circular(30)),
+                    child: TextFormField(
+                      controller: passwordController,
+                      validator: (value) => formValidator(value, 'Password'),
+                      keyboardType: TextInputType.number,
+                      inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                      onFieldSubmitted: (_) => onSubmitHandler(
+                          () => const routes.HomeRoute().go(context)),
+                      obscureText: isUnVisible.value,
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                        hintText: 'Your password',
+                        prefixIcon: Material(
+                          elevation: 0,
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(30)),
+                          child: Icon(
+                            Icons.lock,
+                            color: Theme.of(context).primaryColor,
+                          ),
+                        ),
+                        suffixIcon: IconButton(
+                          icon: Icon(isUnVisible.value
+                              ? Icons.visibility
+                              : Icons.visibility_off),
+                          onPressed: () {
+                            isUnVisible.value = !isUnVisible.value;
+                          },
+                        ),
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 16),
+                if (isFailure.value)
+                  const Text(
+                    'Student number or Password is incorrect',
+                    style: TextStyle(color: Colors.red),
+                  ),
+                const SizedBox(height: 16),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 32),
+                  child: Container(
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      borderRadius: const BorderRadius.all(
+                        Radius.circular(100),
+                      ),
+                      color: Theme.of(context).primaryColor,
+                    ),
+                    child: TextButton(
+                      child: const Text(
+                        "Sign in",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w700,
+                          fontSize: 18,
+                        ),
+                      ),
+                      onPressed: () => onSubmitHandler(
+                        () => const routes.HomeRoute().go(context),
+                      ),
+                    ),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {
+                    openUrl("https://falcon.ecc.ac.jp/eccmo/");
+                  },
+                  child: Text(
+                    "Forgot Password ?",
+                    style: TextStyle(
+                      color: Theme.of(context).primaryColor,
+                      fontSize: 12,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
                 ),
               ],
             ),
-            SizedBox(
-              height: 420,
-              child: Align(
-                alignment: Alignment.bottomCenter,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(40.0)),
-                    backgroundColor: Colors.blue,
-                  ),
-                  onPressed: () {},
-                  child: const Text("Login",
-                      style: TextStyle(color: Colors.white70)),
-                ),
-              ),
-            )
-          ],
-        ),
-      );
-    }
-
-    Widget _buildPageContent(BuildContext context) {
-      return Container(
-        color: Colors.blue.shade200,
-        child: ListView(
-          children: <Widget>[
-            const SizedBox(
-              height: 30.0,
-            ),
-            const CircleAvatar(
-              maxRadius: 50,
-              backgroundColor: Colors.transparent,
-              child: Text('data'),
-            ),
-            const SizedBox(
-              height: 20.0,
-            ),
-            _buildLoginForm(),
-          ],
-        ),
-      );
-    }
-
-    return Scaffold(
-      body: _buildPageContent(context),
+          )
+        ],
+      ),
     );
   }
 }
